0354667a26f8748cf9627600aa4228b7bf6adf56
edward <edwardlfish@gmail.com>
2026-07-13 18:27:11 +0300

    Add user account routes and controller
    - Introduced `userAccount.routes.js` for handling account-related endpoints.
    - Added `userAccount.controller.js` with methods for registration, role management, user updates, and retrieval.
    - Registered new routes under `/account` in `server.js`.

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import config from './configuration/config.js';
 import mongoose from "mongoose";
 import postRoutes from './routes/post.routes.js';
+import userRoutes from "./routes/userAccount.routes.js";
 import errorHandler from "./middlewares/error.middleware.js";
 
 const app = express();
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/forum', postRoutes);
+app.use('/account', userRoutes);
 
 app.use(errorHandler);
 
